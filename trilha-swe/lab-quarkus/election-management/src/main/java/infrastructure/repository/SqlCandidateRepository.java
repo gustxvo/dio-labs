5ce2c7d8fb7a12456bc5cd7c14b2e8a6a93a4bfb
gustxvo <gualmeida2004@gmail.com>
2024-09-08 18:18:16 -0300

    Implementação serviço election-management

---
+++ trilha-swe/lab-quarkus/election-management/src/main/java/infrastructure/repository/SqlCandidateRepository.java
@@ -0,0 +1,23 @@
+package infrastructure.repository;
+
+import domain.Candidate;
+import domain.CandidateQuery;
+import domain.CandidateRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class SqlCandidateRepository implements CandidateRepository {
+
+    @Override
+    public void save(List<Candidate> candidates) {
+
+    }
+
+    @Override
+    public List<Candidate> find(CandidateQuery query) {
+        return List.of();
+    }
+
+}
